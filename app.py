f7212a905d26480c6d9801499a3deef424ee6103
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-04-20 21:05:41 +0530

    created flask api

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import joblib
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Load the trained model
+model = joblib.load('model.pkl')
+
+# Define a route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get keyword from request
+    keyword = request.json.get('keyword')
+
+    # Make prediction
+    predicted_code = model.predict([keyword])
+
+    # Return prediction
+    return jsonify({'predicted_code': predicted_code[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
